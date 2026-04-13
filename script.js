a6556d92217a93eca92f35b57f141593fce3c677
heyitsmeravi <ravijnv2428@gmail.com>
2026-04-13 11:16:30 +0530

    filter logic added

--- script.js
+++ script.js
@@ -28,3 +28,62 @@ window.addEventListener("scroll", function(){
     }
     lastScroll=window.scrollY;
 });
+
+// Set current year in footer
+let yearSpan=document.getElementById("current-year");
+let currentYear=new Date().getFullYear();
+yearSpan.textContent=currentYear;
+
+// Filter functionality
+const products = document.querySelectorAll(".product-section-card");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const radios = document.querySelectorAll('input[name="price"]');
+
+function filterProducts() {
+    let selectedCategories = [];
+    let selectedPrice = null;
+
+    // categories
+    checkboxes.forEach(cb => {
+        if (cb.checked) {
+            selectedCategories.push(cb.value);
+        }
+    });
+
+    // price
+    radios.forEach(r => {
+        if (r.checked) {
+            selectedPrice = r.value;
+        }
+    });
+
+    products.forEach(product => {
+        const category = product.dataset.category;
+        const price = parseInt(product.dataset.price);
+
+        let categoryMatch =
+            selectedCategories.length === 0 ||
+            selectedCategories.includes(category);
+
+        let priceMatch = true;
+
+        if (selectedPrice) {
+            const [min, max] = selectedPrice.split("-").map(Number);
+            priceMatch = price >= min && price <= max;
+        }
+
+        if (categoryMatch && priceMatch) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+}
+
+checkboxes.forEach(cb => {
+    cb.addEventListener("change", filterProducts);
+});
+
+radios.forEach(r => {
+    r.addEventListener("change", filterProducts);
+});
